d3411858f2194a4bdc106c640fb6cfcdd026bc5d
August Alexandersson <augustalexandersson@gmail.com>
2017-07-14 21:39:31 +0200

    . . .

--- server/test.js
+++ server/test.js
@@ -4,10 +4,13 @@ var io = require('socket.io')(http)
 
 app.set('port', 8080);
 
+let clients = Set()
+
 io.on('connection', (socket) => {
-    console.log(socket);
+    clients.add(socket.handshake.address)
     socket.on('agge', data => {
         console.log(`Got data on channel "agge": ${data}`);
+        socket.emit('agge', data);
     })
     console.log('a user connected');
 })
