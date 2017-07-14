04f60f52c97fbd4cc1fae2d33538ac45d91d7440
Samuel Utbult <samuel.utbult@oborgen.se>
2017-07-15 00:01:50 +0200

    Server implementation begin

--- server/test.js
+++ server/test.js
@@ -4,32 +4,91 @@ var io = require('socket.io')(http)
 
 app.set('port', 8081);
 
-let clients = new Set()
+function genColorPart() {
+    return Math.round(Math.random() * 255)
+}
+function genColor() {
+    return [
+        genColorPart(),
+        genColorPart(),
+        genColorPart(),
+    ]
+}
 
-let colors = [
-    [255,0,0],
-    [0,255,0],
-    [0,0,255]
-]
+var clientIdCounter = 1;
+var clients = {};
+
+function genClientId() {
+    const clientId = clientIdCounter
+    clientIdCounter++
+    return clientId.toString()
+}
+function broadcast(clientId, message) {
+    for(var otherClientId in client) {
+        if(clientId !== otherClientId) {
+            const client = clients[otherClientId]
+            client.socket.emit("agge", JSON.stringify(message))
+        }
+    }
+}
+function broadcastMove(clientId, data) {
+    broadcast(clientId, {
+        command: "movePlayer",
+        x: data.x,
+        y: data.y,
+    })
+}
+function genPosPart() {
+    return Math.round(Math.random() * 1000)
+}
+function broadcastCreatePlayerData(clientId, data) {
+    data = Object.assign({}, data, {
+        command: "createPlayer",
+    })
+    broadcast(null, data)
+}
+function broadcastCreatePlayer(clientId) {
+    broadcastCreatePlayerData({
+        clientId: clientId,
+        w: 10,
+        h: 10,
+        x: genPosPart(),
+        y: genPosPart(),
+        speed: 5,
+        color: genColor,
+    })
+}
+function broadcastRequestPlayer(clientId) {
+    broadcast(clientId, {
+        command: 'requestPlayer',
+        clientId: clientId,
+    })
+}
 
 io.on('connection', (socket) => {
-    clients.add(socket)
-    let color = colors.pop()
-    colors.unshift(color)
+    const clientId = genClientId();
+    clients[clientId] = {
+        socket: socket,
+    }
     socket.on('agge', data => {
-        let newData = {
-            rect: JSON.parse(data),
-            color
+        data = JSON.parse(data)
+        switch(data.command) {
+            case "move":
+                broadcastMove(clientId, data)
+                break
+
+            case 'responsePlayer':
+                broadcastCreatePlayerData(data.clientId, data)
+                break
         }
-        let pack = JSON.stringify(newData)
-        console.log(`Got data on channel "agge": ${pack}`);
-        clients.forEach(client => {
-            // if(client === socket) return
-            
-            client.emit('agge', pack)
-        })
     })
-    console.log(`a user connected: ${socket.handshake.address}`);
+    socket.emit("agge", JSON.stringify({
+        command: "handshake",
+        clientId: clientId,
+    }))
+    broadcastCreatePlayer(clientId)
+    broadcaseRequestPlayer(clientId)
+    console.log(`a user connected: ${socket.handshake.address} that was given this id: ${clientId}`);
 })
 
 http.listen(8081, '0.0.0.0', () => {
