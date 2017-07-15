7e6571a5e536d90ec37b251f731c2fa19f2dc2fe
josef <josef.utbult@hotmail.com>
2017-07-16 00:02:40 +0200

    Enabled more than one rectangle in each object.

--- client/logic/Houses.js
+++ client/logic/Houses.js
@@ -19,6 +19,17 @@ module.exports = function (deps) {
                 b: color[2]
             })
             ctx.fillRect(rect.x, rect.y, rect.w, rect.h)
+
+            let details = house.details
+            for(let detail of details){
+                let color = detail.color
+                setColor({
+                    r: color[0],
+                    g: color[1],
+                    b: color[2]
+                })
+                ctx.fillRect(rect.x + detail.relX, rect.y + detail.relY, detail.w, detail.h)
+            }
         }
     }
 }
