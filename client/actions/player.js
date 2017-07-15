618d200e6e14acdc95ca4117d4ad7de3cd0c1ccb
josef <josef.utbult@hotmail.com>
2017-07-15 23:49:38 +0200

    Enabled more than one rectangle in each object.

--- client/actions/player.js
+++ client/actions/player.js
@@ -12,13 +12,35 @@ module.exports = {
                 h: 10,
             },
             details: [{
-                relX: 0,
-                relY: 8,
-                w: 10,
-                h: 2,
-                color:[0, 0, 0]
-            }],
-            color:[255, 0, 0],
+                    relX: 0,
+                    relY: 2,
+                    w: 10,
+                    h: 2,
+                    color:[0, 170, 170]
+                },
+                {
+                    relX: 0,
+                    relY: 4,
+                    w: 10,
+                    h: 2,
+                    color:[0, 255, 0]
+                },
+                {
+                    relX: 0,
+                    relY: 6,
+                    w: 10,
+                    h: 2,
+                    color:[170, 170, 0]
+
+                },
+                {
+                    relX: 0,
+                    relY: 8,
+                    w: 10,
+                    h: 2,
+                    color:[255, 0, 0]
+                }],
+            color:[0, 0, 255],
             speed: 20,
             clientId
         })
