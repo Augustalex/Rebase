62555a984326e4cdce0821788e612489445a1e44
josef <josef.utbult@hotmail.com>
2017-07-16 00:01:04 +0200

    Enabled more than one rectangle in each object.

--- client/actions/building.js
+++ client/actions/building.js
@@ -2,6 +2,7 @@
 module.exports = {
     createHouse(state, {clientId}, { selector, setter }){
         let player = selector.getPlayerWithId(clientId)
+        let flagColor = [...selector.getPlayerWithId(clientId).color]
         
         return setter.addHouse(state, {
             rect:{
@@ -10,6 +11,19 @@ module.exports = {
                 w: 25,
                 h: 25
             },
+            details: [{
+                relX: 10,
+                relY: 5,
+                w: 2,
+                h: 10,
+                color:[0, 0, 0]
+            },{
+                relX: 12,
+                relY: 7,
+                w: 10,
+                h: 5,
+                color: flagColor
+            }],
             color:[153, 76, 0],
             clientId: clientId
         })
